cc4d923640abcfd542704ca821d3e8c35c4103a3
cromatical <lyh940830@gmail.com>
2020-12-23 02:48:16 +0900

    2020-12-23 / fail

---
+++ Algorithm/개구리 뛰기(x).py
@@ -0,0 +1,20 @@
+def solution(lst, i, cnt):
+	print(lst, "점프 횟수 :", cnt, "징검다리 위치 :", i, "점프거리 :", lst[i])
+	if lst[-1] == i: # 마지막 징검다리 도착
+		return cnt
+
+	if (len(lst) < i) or (i > 0): # 범위를 벗어 날 때
+		return -1
+	else:
+		if not lst[i]: # 0일 때 움직일수 없는 경우
+			return -1  
+		if (not len(lst) < i) and (not i > 0):
+			i += lst[i]
+			solution(lst, i, cnt + 1)
+			i -= lst[i]
+			solution(lst, i, cnt + 1)
+
+
+nums = list(map(int, input().split()))
+
+print(solution(nums, 0, 0))
