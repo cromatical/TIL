109586ea8444bbec70452e9bdb2b75b55a160835
cromatical <lyh940830@gmail.com>
2021-01-02 00:52:43 +0900

    2021-01-02 / fail

--- Algorithm-Python/떡볶이 떡 만들기.py
+++ Algorithm-Python/떡볶이 떡 만들기.py
@@ -3,7 +3,10 @@
 n, m = map(int, input().split())
 
 lst = list(map(int, input().split()))
-total = []
+total_lst = []
+
+start = 0
+end = max(lst)
 
 def solution(array, start, end, m):
 	if start > end: # 이진탐색 끝나는 조건
@@ -13,8 +16,8 @@ def solution(array, start, end, m):
 	if total < m: # total이 떡볶이 길이보다 짧은 경우
 		solution(array, start, mid - 1, m)
 	else: # total이 떡볶이 길보다 같거나 긴 경우
-		total.append(mid) # 최대한 덜 잘랐을 때가 정답이므로 여기에서 기록!
+		total_lst.append(mid) # 최대한 덜 잘랐을 때가 정답이므로 여기에서 기록!
 		solution(array, mid + 1, end, m)
 
 solution(lst, start, end, m)
-print(max(total))
+print(max(total_lst))
