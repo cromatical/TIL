8a9b6d29cbcd5899ebf139520f894723b64ae4b2
cromatcal <lyh940830@gmail.com>
2021-01-09 01:44:43 +0900

    2021-01-09 / success

--- Algorithm-Python/Baekjoon 9370 미확인도착지.py
+++ Algorithm-Python/Baekjoon 9370 미확인도착지.py
@@ -37,13 +37,18 @@ for _ in range(T):
 	goal_lst = [int(input()) for _ in range(t)] # 목적지 후보
 
 	success = []
-	for goal in goal_lst: # s -> g -> h -> goal 최단경로를 확인할 때
+	for goal in goal_lst:
 		result1 = dijkstra(s, g) + dijkstra(g, h) + dijkstra(h, goal) # s -> g -> h -> goal 최단경로를 확인할 때
 		result2 = dijkstra(s, h) + dijkstra(h, g) + dijkstra(g, goal) # s -> h -> g -> goal 최단경로를 확인할 때
 		result = min(result1, result2)
-		if goal not in success: 
-			if (result < INF and result == dijkstra(s, goal)): # 목적지까지 우회하지 않고 최단거리로 이동할꺼기 때문에 비교
-				success.append(goal) 
+		if (result < INF and result == dijkstra(s, goal)): # 목적지까지 우회하지 않고 최단거리로 이동할꺼기 때문에 비교
+			success.append(goal) 
+
+	for goal in goal_lst:
+		result1 = dijkstra(s, g) + dijkstra(g, h) + dijkstra(h, goal)
+		if goal not in success:
+			if (result < INF):
+				
 
 	success.sort() # 오름차순으로 정렬
 	for val in success:
