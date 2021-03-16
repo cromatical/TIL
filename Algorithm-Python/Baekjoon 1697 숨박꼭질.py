71fb8e702bf608e90fc109c4829175a83b89ba77
cromatcal <lyh940830@gmail.com>
2021-03-17 00:26:34 +0900

    [알고리즘 문제]: BFS / 실패

--- Algorithm-Python/Baekjoon 1697 숨박꼭질.py
+++ Algorithm-Python/Baekjoon 1697 숨박꼭질.py
@@ -6,21 +6,16 @@ n, k = map(int, input().split()) # 수빈이 위치, 동생의 위치
 
 def bfs(n, k, cnt):
 	q = deque([[n]])
-	i = 0
-	while (i < 5):
+	while (q):
 		now = q.popleft()
-		print(now)
 		lst = []
 		for i in now:
-			print(i)
 			move_lst = [i - 1, i + 1, i * 2]
 			for move in move_lst:
 				if move == k:
 					return cnt
 				lst.append(move)
-		print(lst)
 		q.append(lst)
-		cnt += 1
-		i += 1
+		cnt += 1	
 
-print(bfs(n, k, 0))
+print(bfs(n, k, 1))
