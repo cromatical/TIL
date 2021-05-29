8e3302f6053aca6b28560780baee0dabdc4718c6
cromatical <lyh940830@gmail.com>
2021-05-30 03:45:57 +0900

    study: add softmaxwithloss

--- DL_study/activation_function.py
+++ DL_study/activation_function.py
@@ -32,6 +32,25 @@ class sidmoid:
 
 		return dx
 
+class SoftmaxWithLoss:
+	def __init__(self):
+		self.loss = None
+		self.y = None # softmax 출력
+		self.t = None # 정답 레이블
+
+	def forward(self, x, t):
+		self.t = t
+		self.y = softmax(x)
+		self.loss = cross_entropy_error(self.y, self.t)
+
+		return self.loss
+
+	def backward(self, dout=1):
+		batch_size = self.t.shape[0]
+		dx = (self.y - self.t) / batch_size
+
+		return dx
+
 if __name__ == '__main__':
 	# check mask of relu
 	x = np.array([[1.0, -0.5], [-2.0, 3.0]])
