c8af7ac54a5f4d1196b3153abf5bac5fa0f9b0a5
cromatical <lyh940830@gmail.com>
2021-05-30 03:26:23 +0900

    study: add affine layer

--- DL_study/layer.py
+++ DL_study/layer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Mullayer:
 	def __init__(self):
 		self.x = None
@@ -30,3 +32,24 @@ class Addlayer:
 		dy = dout * 1
 
 		return dx, dy
+
+class Affine:
+	def __init__(self, W, b):
+		self.W = W
+		self.b = b
+		self.x = None
+		self.y = None
+		self.db = None
+	
+	def forward(self, x):
+		self.x = x
+		out = np.dot(x, self.W) + self.b
+
+		return out
+
+	def backward(self, dout):
+		dx = np.dot(dout, self.W.T)
+		self.dW = np.dot(self.x.T, dout)
+		self.db = np.sum(dout, axis=0)
+
+		return dx
